f45a9fa9f63d21c351f3ab4a5cacef3d8a335561
Daniel Cothran <daniel@andile.co>
2020-01-28 10:09:29 -0500

    initial commit

---
+++ src/Plugin/views/field/Price.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\commerce_views_connector\Plugin\views\field;
+
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+
+/**
+ * Displays taxonomy term names and allows converting spaces to hyphens.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("commerce_views_connector_price")
+ */
+class Price extends FieldPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+  }
+
+  public function getValue(ResultRow $values, $field = NULL) {
+    $entity = $this->getEntity($values);
+    $values = current($entity->get('price')->first()->toPrice());
+
+    return $values;
+  }
+
+  public function advancedRender(ResultRow $values) {
+    $entity = $this->getEntity($values);
+    $values = current($entity->get('price')->first()->toPrice());
+
+    return $values;
+  }
+
+}
